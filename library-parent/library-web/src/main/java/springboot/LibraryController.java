04dabdb8247c183fb997d2c313e7a1fab5c32c8a
4Blam <sashablam2001@gmail.com>
2023-02-27 17:33:36 +0700

    Set swagger, changed mapping

--- library-parent/library-web/src/main/java/springboot/LibraryController.java
+++ library-parent/library-web/src/main/java/springboot/LibraryController.java
@@ -2,10 +2,8 @@ package springboot;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 
@@ -18,50 +16,52 @@ public class LibraryController {
     public LibraryController(LibraryWebCore libraryWebCore){
         this.libraryWebCore = libraryWebCore;
     }
-    @GetMapping("/getAllBooks")
+    @GetMapping("/Books")
     public List<BookWeb> getAllBooks() { return libraryWebCore.getAllBooks(); }
-    @GetMapping("/getBookById")
+    @GetMapping("/Books/Id")
     public List<BookWeb> getBookById(@RequestParam(required = true) int id){
         return libraryWebCore.getBookById(id);
     }
-    @GetMapping("/getBooksByAuthor")
+    @GetMapping("/Books/Author")
     public List<BookWeb> getBooksByAuthor(@RequestParam(required = true) String author){
         return libraryWebCore.getBooksByAuthor(author);
     }
-    @GetMapping("/getBookByTitle")
+    @GetMapping("/Books/Title")
     public List<BookWeb> getBooksByTitle(@RequestParam(required = true) String title){
         return libraryWebCore.getBookByTitle(title);
     }
-    @GetMapping("/updateAuthor")
+    @PutMapping("/Books/Author")
     public String updateAuthor(@RequestParam(required = true) int id,
                                @RequestParam(required = true) String author){
         libraryWebCore.updateBookById(id, "author", author);
         return "Successfully updated book with id = " + id + ", changed author to " + author;
     }
-    @GetMapping("/updateTitle")
+    @PutMapping("/Books/Title")
     public String updateTitle(@RequestParam(required = true) int id,
                                @RequestParam(required = true) String title){
         libraryWebCore.updateBookById(id, "title", title);
         return "Successfully updated book with id = " + id + ", changed title to " + title;
     }
-    @GetMapping("/updatePublished_in")
+    @PutMapping("/Books/PublishedIn")
     public String updatePublished_in(@RequestParam(required = true) int id,
                                @RequestParam(required = true) int published_in){
         libraryWebCore.updateBookById(id, "published_in", "" + published_in);
         return "Successfully updated book with id = " + id + ", changed published_in to " + published_in;
     }
-    @GetMapping("/insertBook")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/Books")
     public String insert(@RequestParam(required = true) String title,
                                @RequestParam(required = true) String author,
                                @RequestParam(required = true) int published_in ){
         libraryWebCore.insertBook(title, author, published_in);
         return "Successfully inserted a book with title = " + title + ", author = " + author + ", published in = " + published_in;
     }
-    @GetMapping("/deleteBook")
+    @DeleteMapping("/Books/Id")
     public String delete(@RequestParam(required = true) int id){
         libraryWebCore.deleteBookById(id);
         return "Successfully deleted a book with id = " + id;
     }
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     @ExceptionHandler(RuntimeException.class)
     public RedirectView handleError(Exception ex) {
         log.error(ex.getMessage());
--- library-parent/library-web/src/main/java/springboot/LibraryErrorController.java
+++ library-parent/library-web/src/main/java/springboot/LibraryErrorController.java
@@ -1,12 +1,15 @@
 package springboot;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LibraryErrorController implements ErrorController {
     @GetMapping("/error")
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
     String error() {
         return "Sorry, internal error has occured";
     }
